7cf274447aa0e9a388e0c1d05fa757298ae86f09
HatcheROfficials <hatcher.officials@gmail.com>
2023-02-06 22:09:33 +0530

    Implemented Scroll Button

--- indexJs.js
+++ indexJs.js
@@ -5,7 +5,10 @@ var imgArr = ["https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/new-
 "https://carwow-uk-wp-3.imgix.net/18015-MC20BluInfinito-scaled-e1666008987698.jpg",
 "https://carnetwork.s3.ap-southeast-1.amazonaws.com/file/285b15eb44cd4df7a747e051383638a7.jpg",
 "https://imgcdnblog.carmudi.com.ph/wp-content/uploads/2021/08/18102413/maserati-mc20-1.png",
-"https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Porsche-Taycan-190820221318.jpg&w=872&h=578&q=75&c=1"];
+"https://cdni.autocarindia.com/utils/imageresizer.ashx?n=https://cms.haymarketindia.net/model/uploads/modelimages/Porsche-Taycan-190820221318.jpg&w=872&h=578&q=75&c=1",
+"https://cdn.luxe.digital/media/20230105073805/fastest-cars-world-2023-list-ranking-luxe-digital.jpg",
+"https://www.cnet.com/a/img/resize/50e11858414030f7ace1dd3d92f7d24e5f4c5ce8/hub/2023/01/17/91eb6502-7246-430f-aa5f-e99f7cdea3ac/rs-2024-chevrolet-corvette-e-ray-3lz-006.jpg?auto=webp&fit=crop&height=620&width=620",
+"https://www.bugatti.com/fileadmin/_processed_/e/e/csm_og-image_506cf6a92e.jpg"];
 
 // getting all image tags. First one is main, rest 5 are thumbnail images
 var images = document.getElementsByTagName("img");
@@ -17,7 +20,7 @@ for(let i=0; i<5; i++){
     images[i+1].src = imgArr[i];
 }
 
-// Selecting thumbnail image will display in main image   
+// Selected thumbnail image will display in main image   
 function setMainImage(event){
     var targetImg = event.target;
     images[0].src = targetImg.src;
@@ -25,3 +28,32 @@ function setMainImage(event){
 
 var thumbnailDiv = document.getElementById("thumbBar");
 thumbnailDiv.addEventListener("click",setMainImage);
+
+// Scroll button function
+var l_scrollBtn = document.getElementById("leftBtn");
+var r_scrollBtn = document.getElementById("rightBtn");
+
+// index from which images will be taken from image array
+var imgArrStartIndex = 0; // start index
+var imgArrEndIndex = 4; // end index
+
+function scrollThumbnail(event){
+    if(event.target == r_scrollBtn && imgArrEndIndex < imgArr.length-1){
+        imgArrStartIndex++;
+        imgArrEndIndex++;
+
+        for(let i=imgArrStartIndex; i<=imgArrEndIndex; i++){
+            images[i-imgArrStartIndex+1].src = imgArr[i];
+        }
+    } else if(event.target == l_scrollBtn && imgArrStartIndex > 0){
+        imgArrStartIndex--;
+        imgArrEndIndex--;
+
+        for(let i=imgArrStartIndex; i<=imgArrEndIndex; i++){
+            images[i-imgArrStartIndex+1].src = imgArr[i];
+        }
+    }
+}
+
+r_scrollBtn.addEventListener("click",scrollThumbnail);
+l_scrollBtn.addEventListener("click",scrollThumbnail);
